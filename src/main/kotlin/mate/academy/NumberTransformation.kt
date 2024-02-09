aab92057c6e6769dc6faca2f77a0bc03c137ddfb
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2024-02-09 11:04:41 +0200

    realized calculateTotalPrice and getReminder classes

--- src/main/kotlin/mate/academy/NumberTransformation.kt
+++ src/main/kotlin/mate/academy/NumberTransformation.kt
@@ -1,11 +1,10 @@
 package mate.academy
 
-/*
-    Implement the function. The input String may contain number or be null.
-    If the number is present, multiply it by 3, then add 10 and return the reminder of dividing by 11.
-    If the number is not present return null
- */
+const val THREE = 3
+const val TEN = 10
+const val ELEVEN = 11
 
 fun getReminder(numberStr: String?) : Int? {
-    return null
+    val intValue : Int? = numberStr?.toInt()
+    return intValue?.times(THREE)?.plus(TEN)?.rem(ELEVEN)
 }
