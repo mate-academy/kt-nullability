cfacb17b3bae273dee7b428e303a58b17b425507
Vasyl Piznak <piznak3145@gmail.com>
2024-03-01 21:10:58 +0200

    First commit

--- src/main/kotlin/mate/academy/NumberTransformation.kt
+++ src/main/kotlin/mate/academy/NumberTransformation.kt
@@ -5,7 +5,10 @@ package mate.academy
     If the number is present, multiply it by 3, then add 10 and return the reminder of dividing by 11.
     If the number is not present return null
  */
+const val MULTIPLY = 3
+const val ADD = 10
+const val DIV = 11
 
 fun getReminder(numberStr: String?) : Int? {
-    return null
+    return numberStr?.toInt()?.times(MULTIPLY)?.plus(ADD)?.mod(DIV)
 }
--- src/main/kotlin/mate/academy/TotalPrice.kt
+++ src/main/kotlin/mate/academy/TotalPrice.kt
@@ -4,6 +4,8 @@ package mate.academy
     Calculate the products total price based on the provide price per product and products amount.
     If the price is not provided, use default price per product equal to 29.99
  */
+
+const val DEFAULT_PRICE = 29.99
 fun calculateTotalPrice(pricePerProduct: Double?, count: Int) : Double {
-    return 0.0
+    return pricePerProduct?.times(count) ?: (DEFAULT_PRICE * count)
 }
