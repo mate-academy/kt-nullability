2b8a1dfb1f89f952f840827731c4cd72f2c9fa37
literandltx <romanskovronn@gmail.com>
2024-04-08 13:14:34 +0300

    fix

--- src/main/kotlin/mate/academy/TotalPrice.kt
+++ src/main/kotlin/mate/academy/TotalPrice.kt
@@ -4,8 +4,9 @@ package mate.academy
     Calculate the products total price based on the provide price per product and products amount.
     If the price is not provided, use default price per product equal to 29.99
  */
-fun calculateTotalPrice(pricePerProduct: Double?, count: Int) : Double {
-    val price = pricePerProduct?: 29.99
 
-    return price * count
+const val DEFAULT_PRICE : Double = 29.99
+
+fun calculateTotalPrice(pricePerProduct: Double?, count: Int) : Double {
+    return (pricePerProduct?: DEFAULT_PRICE) * count
 }
