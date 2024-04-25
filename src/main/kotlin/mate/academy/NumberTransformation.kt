9d925a2291a1938e8dfaa1baf21345edff6d3316
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-04-25 07:28:37 +0300

    NumberTransformation.kt

--- src/main/kotlin/mate/academy/NumberTransformation.kt
+++ src/main/kotlin/mate/academy/NumberTransformation.kt
@@ -1,7 +1,11 @@
 package mate.academy
 
+const val MULTIPLIER = 3
+const val ADDEND = 10
+const val MODULUS = 11
+
 fun getReminder(numberStr: String?): Int? {
     return numberStr?.toIntOrNull()?.let {
-        (it * 3 + 10) % 11
+        (it * MULTIPLIER + ADDEND) % MODULUS
     }
 }
