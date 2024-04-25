c8f679c8478592172e10f71e659d90a26682ba07
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-04-25 07:28:51 +0300

    TotalPrice.kt

--- src/main/kotlin/mate/academy/TotalPrice.kt
+++ src/main/kotlin/mate/academy/TotalPrice.kt
@@ -1,6 +1,8 @@
 package mate.academy
 
+const val DEFAULT_PRICE = 29.99
+
 fun calculateTotalPrice(pricePerProduct: Double?, count: Int): Double {
-    val price = pricePerProduct ?: 29.99
+    val price = pricePerProduct ?: DEFAULT_PRICE
     return price * count
 }
