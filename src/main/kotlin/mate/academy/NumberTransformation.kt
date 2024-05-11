7afa66650fe9bf9e9d16a212982bfa73a9cf24d5
Petr <smp22pl@gmail.com>
2024-05-11 21:22:51 +0200

    implement methods

--- src/main/kotlin/mate/academy/NumberTransformation.kt
+++ src/main/kotlin/mate/academy/NumberTransformation.kt
@@ -1,5 +1,9 @@
 package mate.academy
 
+const val MULTIPLIER = 3
+const val ADDITIONAL_CONSTANT = 10
+const val MODULO_DIVISOR = 11
+
 /*
     Implement the function. The input String may contain number or be null.
     If the number is present, multiply it by 3, then add 10 and return the reminder of dividing by 11.
@@ -7,5 +11,5 @@ package mate.academy
  */
 
 fun getReminder(numberStr: String?) : Int? {
-    return null
+    return numberStr?.toInt()?.times(MULTIPLIER)?.plus(ADDITIONAL_CONSTANT)?.rem(MODULO_DIVISOR)
 }
