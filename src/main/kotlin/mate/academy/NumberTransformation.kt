257b7651be03e89abd4ff58a46e758cd8a107402
Artem Kostenko <a_kostenco@yahoo.com>
2024-05-15 20:35:56 +0300

    Nullability solution first commit

--- src/main/kotlin/mate/academy/NumberTransformation.kt
+++ src/main/kotlin/mate/academy/NumberTransformation.kt
@@ -1,5 +1,8 @@
 package mate.academy
 
+const val MULTIPLIER = 3
+const val ADDER = 10
+const val DIVIDER = 11
 /*
     Implement the function. The input String may contain number or be null.
     If the number is present, multiply it by 3, then add 10 and return the reminder of dividing by 11.
@@ -7,5 +10,6 @@ package mate.academy
  */
 
 fun getReminder(numberStr: String?) : Int? {
-    return null
+    val value = numberStr?.toInt()
+    return value?.times(MULTIPLIER)?.plus(ADDER)?.rem(DIVIDER)
 }
