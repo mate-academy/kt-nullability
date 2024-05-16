1b5ec46c7677d82a3fcb6594d4442121d0d622cd
Yuriy.Khilko <yurakhilko@gmail.com>
2024-05-16 18:56:45 +0300

    added solution

--- src/main/kotlin/mate/academy/NumberTransformation.kt
+++ src/main/kotlin/mate/academy/NumberTransformation.kt
@@ -1,11 +1,14 @@
 package mate.academy
 
+const val TIMES = 3
+const val PLUS = 10
+const val REM = 11
 /*
     Implement the function. The input String may contain number or be null.
     If the number is present, multiply it by 3, then add 10 and return the reminder of dividing by 11.
     If the number is not present return null
  */
 
-fun getReminder(numberStr: String?) : Int? {
-    return null
+fun getReminder(numberStr: String?): Int? {
+    return numberStr?.toInt()?.times(TIMES)?.plus(PLUS)?.rem(REM);
 }
