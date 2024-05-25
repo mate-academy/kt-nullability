db970d4e69e9f4922237063487009cd33cb91390
Dmitri Kolos <skil@bigmir.net>
2024-05-25 22:39:26 +0300

    nullability solution

--- src/main/kotlin/mate/academy/NumberTransformation.kt
+++ src/main/kotlin/mate/academy/NumberTransformation.kt
@@ -6,6 +6,10 @@ package mate.academy
     If the number is not present return null
  */
 
+const val MULTIPLIER = 3
+const val ADDED = 10
+const val REMINDER = 11
+
 fun getReminder(numberStr: String?) : Int? {
-    return null
+    return numberStr?.toInt()?.times(MULTIPLIER)?.plus(ADDED)?.rem(REMINDER)
 }
