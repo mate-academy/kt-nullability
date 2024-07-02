abc5443ce9821a8c7a0e19c7598b03b08c6b890d
Denis Kalyakin <denis.kalyakin999@gmail.com>
2024-07-02 15:37:23 +0300

    consts was added to NumberTransformation

--- src/main/kotlin/mate/academy/NumberTransformation.kt
+++ src/main/kotlin/mate/academy/NumberTransformation.kt
@@ -6,6 +6,10 @@ package mate.academy
     If the number is not present return null
  */
 
+const val MULTIPLIER = 3
+const val ADDITION = 10
+const val DIVIDER = 11
+
 fun getReminder(numberStr: String?) : Int? {
-    return numberStr?.toInt()?.times(3)?.plus(10)?.rem(11)
+    return numberStr?.toInt()?.times(MULTIPLIER)?.plus(ADDITION)?.rem(DIVIDER)
 }
