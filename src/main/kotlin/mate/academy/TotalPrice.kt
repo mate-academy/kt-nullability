f15b777d8b078e19b550ff4110e2cd2096357836
TonyH277 <gajjduktoxa@gmail.com>
2024-07-02 17:13:08 +0300

    implement nullability fun

--- src/main/kotlin/mate/academy/TotalPrice.kt
+++ src/main/kotlin/mate/academy/TotalPrice.kt
@@ -1,9 +1,12 @@
 package mate.academy
 
+private const val DEFAULT = 29.99
+
 /*
     Calculate the products total price based on the provide price per product and products amount.
     If the price is not provided, use default price per product equal to 29.99
  */
 fun calculateTotalPrice(pricePerProduct: Double?, count: Int) : Double {
-    return 0.0
+    val price = pricePerProduct ?: DEFAULT
+    return price * count
 }
