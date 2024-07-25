e3f2104bc07b3f5e9f63843da254b5d825f549bd
Max <mpbakunov@gmail.com>
2024-07-25 21:40:29 +0200

    Fixing bugs #03

--- src/main/kotlin/mate/academy/NumberTransformation.kt
+++ src/main/kotlin/mate/academy/NumberTransformation.kt
@@ -5,11 +5,11 @@ package mate.academy
     If the number is present, multiply it by 3, then add 10 and return the reminder of dividing by 11.
     If the number is not present return null
  */
-val MULT_VALUE = 3
-val ADD_VALUE = 10
-val REM_DIV_VAlue = 11
+const val MULT_VALUE = 3
+const val ADD_VALUE = 10
+const val REM_DIV_VAlUE = 11
 
 fun getReminder(numberStr: String?) : Int? {
     return numberStr?.filter { c -> c.isDigit() }?.toIntOrNull()?.times(MULT_VALUE)
-        ?.plus(ADD_VALUE)?.rem(REM_DIV_VAlue)
+        ?.plus(ADD_VALUE)?.rem(REM_DIV_VAlUE)
 }
