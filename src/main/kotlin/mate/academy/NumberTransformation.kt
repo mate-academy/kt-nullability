0cddac250f5e28e4104e81139f63b00d94168e77
Nikita <kit1datasvit@gmail.com>
2024-10-15 15:20:47 +0300

    - complete task

--- src/main/kotlin/mate/academy/NumberTransformation.kt
+++ src/main/kotlin/mate/academy/NumberTransformation.kt
@@ -6,6 +6,12 @@ package mate.academy
     If the number is not present return null
  */
 
+/*
+    Реализуйте функцию. Входная строка может содержать число или быть нулевой.
+    Если число присутствует, умножьте его на 3, затем добавьте 10 и верните остаток от деления на 11.
+    Если число отсутствует, верните нуль
+ */
+
 fun getReminder(numberStr: String?) : Int? {
-    return null
+    return if (numberStr != "") numberStr?.toInt()?.times(3)?.plus(10)?.rem(11) else null
 }
--- src/main/kotlin/mate/academy/TotalPrice.kt
+++ src/main/kotlin/mate/academy/TotalPrice.kt
@@ -1,9 +1,16 @@
 package mate.academy
+const val defaultPrice = 29.99
 
 /*
     Calculate the products total price based on the provide price per product and products amount.
     If the price is not provided, use default price per product equal to 29.99
  */
+
+/*
+    Рассчитайте общую стоимость товаров на основе предоставленной цены за товар и количества товаров.
+    Если цена не указана, используйте цену за товар по умолчанию, равную 29,99
+ */
+
 fun calculateTotalPrice(pricePerProduct: Double?, count: Int) : Double {
-    return 0.0
+    return pricePerProduct?.times(count) ?: defaultPrice.times(count)
 }
