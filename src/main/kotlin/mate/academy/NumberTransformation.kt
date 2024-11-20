93a51615e0fe7ace7dd36dc552b6841cbb800479
richn <rolik.alexx@gmail.com>
2024-11-21 01:06:53 +0200

    fixed magic numbers

--- src/main/kotlin/mate/academy/NumberTransformation.kt
+++ src/main/kotlin/mate/academy/NumberTransformation.kt
@@ -1,5 +1,7 @@
 package mate.academy
-
+const val MULTIPLY_NUMBER = 3
+const val NUMBER_TO_ADD = 10
+const val DIVIDING_NUMBER = 11
 /*
     Implement the function. The input String may contain number or be null.
     If the number is present, multiply it by 3, then add 10 and return the reminder of dividing by 11.
@@ -8,6 +10,6 @@ package mate.academy
 
 fun getReminder(numberStr: String?) : Int? {
     return numberStr?.toIntOrNull()?.let { number ->
-        (number * 3 + 10) % 11
+        (MULTIPLY_NUMBER * 3 + NUMBER_TO_ADD) % DIVIDING_NUMBER
     }
 }
