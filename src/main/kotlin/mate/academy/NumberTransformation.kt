224bcbebec94ce18489fbbf77c3e49721cf2376a
Roman Voynahiy <romanvoynahiy@gmail.com>
2025-03-13 17:23:20 +0200

    1. Implemented getReminder() function to calculate the reminder based on the number input. 2. Implemented calculateTotalPrice() function with default price logic.

--- src/main/kotlin/mate/academy/NumberTransformation.kt
+++ src/main/kotlin/mate/academy/NumberTransformation.kt
@@ -1,11 +1,11 @@
 package mate.academy
 
-/*
-    Implement the function. The input String may contain number or be null.
-    If the number is present, multiply it by 3, then add 10 and return the reminder of dividing by 11.
-    If the number is not present return null
- */
+const val MULTIPLIER = 3
+const val ADDEND = 10
+const val DIVISOR = 11
 
 fun getReminder(numberStr: String?) : Int? {
-    return null
+    if (numberStr == null) return null
+    val potentialNumber = numberStr.filter { c -> c.isDigit() }.toInt()
+    return potentialNumber.times(MULTIPLIER).plus(ADDEND).rem(DIVISOR)
 }
