4c5c8c9e06a378b34cc4738859f8dd11fdb4e1c6
Oleksandr Kalenyk <carbonmail88@gmail.com>
2025-05-12 14:06:34 +0300

    kt nullability solution

--- src/main/kotlin/mate/academy/NumberTransformation.kt
+++ src/main/kotlin/mate/academy/NumberTransformation.kt
@@ -5,10 +5,10 @@ package mate.academy
     If the number is present, multiply it by 3, then add 10 and return the reminder of dividing by 11.
     If the number is not present return null
  */
+const val three = 3
+const val ten = 10
+const val eleven = 11
 
 fun getReminder(numberStr: String?) : Int? {
-    val three = 3
-    val ten = 10
-    val eleven = 11
     return numberStr?.toInt()?.times(three)?.plus(ten)?.rem(eleven)
 }
--- src/main/kotlin/mate/academy/TotalPrice.kt
+++ src/main/kotlin/mate/academy/TotalPrice.kt
@@ -4,7 +4,9 @@ package mate.academy
     Calculate the products total price based on the provide price per product and products amount.
     If the price is not provided, use default price per product equal to 29.99
  */
+const val thirty = 29.99
+
 fun calculateTotalPrice(pricePerProduct: Double?, count: Int) : Double {
-    val price: Double = pricePerProduct?:29.99
+    val price: Double = pricePerProduct?:thirty
     return price * count
 }
